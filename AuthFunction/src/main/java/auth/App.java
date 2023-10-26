0607947932a7ec1433019213b8a19bcf15642136
Felipe Ribeiro <ribeiro.feu@gmail.com>
2023-10-26 00:36:34 -0300

    Implement Lambda authorize - no validation

--- AuthFunction/src/main/java/auth/App.java
+++ AuthFunction/src/main/java/auth/App.java
@@ -1,36 +1,76 @@
 package auth;
 
+import auth.io.AuthPolicy;
 import auth.service.AuthService;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
-import java.util.HashMap;
-import java.util.Map;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayCustomAuthorizerEvent;
 
 /**
  * Handler for requests to Lambda function.
  */
-public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+public class App implements RequestHandler<APIGatewayCustomAuthorizerEvent, AuthPolicy> {
 
-    public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
-        Map<String, String> headers = new HashMap<>();
-        headers.put("Content-Type", "application/json");
-        headers.put("X-Custom-Header", "application/json");
+    public AuthPolicy handleRequest(final APIGatewayCustomAuthorizerEvent input, final Context context) {
 
-        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
-                .withHeaders(headers);
         try {
-            AuthService authService = new AuthService();
-            String output = authService.getToken(input.getBody());
 
-            return response
-                    .withStatusCode(200)
-                    .withBody(output);
-        } catch (Exception e) {
-            return response
-                    .withBody("{}")
-                    .withStatusCode(500);
+            String principalId = "xxxx";
+
+            String methodArn = input.getMethodArn();
+            String[] arnPartials = methodArn.split(":");
+            String region = arnPartials[3];
+            String awsAccountId = arnPartials[4];
+
+            String[] apiGatewayArnPartials = arnPartials[5].split("/");
+            String restApiId = apiGatewayArnPartials[0];
+            String stage = apiGatewayArnPartials[1];
+            String httpMethod = apiGatewayArnPartials[2];
+            String resource = ""; // root resource
+            if (apiGatewayArnPartials.length == 4) {
+                resource = apiGatewayArnPartials[3];
+            }
+
+            String document = input.getHeaders().get("X-Document");
+
+            AuthService authService = new AuthService();
+            if (authService.validateAccess(document)){
+                return new AuthPolicy(principalId, AuthPolicy.PolicyDocument.getAllowAllPolicy(region, awsAccountId, restApiId, stage));
+            }
+
+            return new AuthPolicy(principalId, AuthPolicy.PolicyDocument.getDenyAllPolicy(region, awsAccountId, restApiId, stage));
+
+        }catch (Exception e ){
+            throw new RuntimeException("Unauthorized");
         }
+
+        // validate the incoming token
+        // and produce the principal user identifier associated with the token
+
+        // this could be accomplished in a number of ways:
+        // 1. Call out to OAuth provider
+        // 2. Decode a JWT token in-line
+        // 3. Lookup in a self-managed DB
+
+
+        // if the client token is not recognized or invalid
+        // you can send a 401 Unauthorized response to the client by failing like so:
+        // throw new RuntimeException("Unauthorized");
+
+        // if the token is valid, a policy should be generated which will allow or deny access to the client
+
+        // if access is denied, the client will receive a 403 Access Denied response
+        // if access is allowed, API Gateway will proceed with the back-end integration configured on the method that was called
+
+        // this function must generate a policy that is associated with the recognized principal user identifier.
+        // depending on your use case, you might store policies in a DB, or generate them on the fly
+
+        // keep in mind, the policy is cached for 5 minutes by default (TTL is configurable in the authorizer)
+        // and will apply to subsequent calls to any method/resource in the RestApi
+        // made with the same token
+
+        // the example policy below denies access to all resources in the RestApi
+
+//        return new AuthPolicy(principalId, AuthPolicy.PolicyDocument.getDenyAllPolicy(region, awsAccountId, restApiId, stage));
     }
 }
--- AuthFunction/src/main/java/auth/service/AuthService.java
+++ AuthFunction/src/main/java/auth/service/AuthService.java
@@ -1,29 +1,17 @@
 package auth.service;
 
-import auth.dao.CustomerDao;
-import auth.model.Customer;
-import auth.model.Request;
-import auth.utils.JPAUtil;
-import auth.utils.JwtUtils;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import javax.persistence.EntityManager;
 
 public class AuthService {
 
-    public String getToken (String body) throws JsonProcessingException {
+    public boolean validateAccess (String body) {
 
-        ObjectMapper oMapper = new ObjectMapper();
-        Request request = oMapper.readValue(body, Request.class);
+//        EntityManager em = JPAUtil.getEntityManager();
+//        CustomerDao dao = new CustomerDao(em);
+//        Customer customer = dao.findByCpf(request.getCpf());
 
-        EntityManager em = JPAUtil.getEntityManager();
-        CustomerDao dao = new CustomerDao(em);
-        Customer customer = dao.findByCpf(request.getCpf());
-
-        if (customer == null ) {
-            throw new RuntimeException("Not Found");
-        }
-        return JwtUtils.genereateToken(customer.getId().toString());
+//        if (customer == null ) {
+//            throw new RuntimeException("Not Found");
+//        }
+        return true;
     }
 }
