1007bf23e4bba528dc6f930879bbf677e5b236d2
Felipe Ribeiro <ribeiro.feu@gmail.com>
2023-11-04 20:00:29 -0300

    set env vars in persistence.xml

--- src/main/java/auth/service/AuthService.java
+++ src/main/java/auth/service/AuthService.java
@@ -1,16 +1,22 @@
 package auth.service;
 
 
+import auth.dao.CustomerDao;
+import auth.model.Customer;
+import auth.utils.JPAUtil;
+
+import javax.persistence.EntityManager;
+
 public class AuthService {
 
     public boolean validateAccess (String document) {
-//        EntityManager em = JPAUtil.getEntityManager();
-//        CustomerDao dao = new CustomerDao(em);
-//        Customer customer = dao.findByCpf(request.getCpf());
+        EntityManager em = JPAUtil.getEntityManager();
+        CustomerDao dao = new CustomerDao(em);
+        Customer customer = dao.findByCpf(document);
 
-//        if (customer == null ) {
-//            throw new RuntimeException("Not Found");
-//        }
-        return document.equals("123");
+        if (customer == null ) {
+            throw new RuntimeException("Not Found");
+        }
+        return true;
     }
 }
